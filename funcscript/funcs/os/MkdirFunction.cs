c80d4b139fe0dec98cb4d6e1b9c59bf9eb334595
tewelde <teweldemat@gmail.com>
2025-01-07 22:26:16 +0300

    mkdir

---
+++ funcscript/funcs/os/MkdirFunction.cs
@@ -0,0 +1,45 @@
+using FuncScript.Core;
+using System;
+using System.IO;
+using FuncScript.Model;
+
+namespace FuncScript.Funcs.OS
+{
+    internal class MkdirFunction : IFsFunction
+    {
+        public CallType CallType => CallType.Prefix;
+
+        public string Symbol => "mkdir";
+
+        public object EvaluateList(FsList pars)
+        {
+            if (pars.Length != 1)
+                return new FsError(FsError.ERROR_PARAMETER_COUNT_MISMATCH,
+                    $"{Symbol} function: invalid parameter count. 1 expected, got {pars.Length}");
+
+            var par0 = pars[0];
+            if (par0 == null)
+                return new FsError(FsError.ERROR_TYPE_INVALID_PARAMETER,
+                    $"Function {Symbol}: parameter cannot be null");
+
+            if (par0 is not string directory)
+                return new FsError(FsError.ERROR_TYPE_INVALID_PARAMETER,
+                    $"Function {Symbol}: Type mismatch. Parameter must be a string");
+
+            try
+            {
+                Directory.CreateDirectory(directory);
+                return directory;
+            }
+            catch (Exception ex)
+            {
+                return new FsError(ex);
+            }
+        }
+
+        public string ParName(int index)
+        {
+            return index == 0 ? "path" : null;
+        }
+    }
+}
